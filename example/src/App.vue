486a1995b60d54594970e6cc16c60b76392bd4fe
GradyYuan256 <jackyuan256@gmail.com>
2022-02-13 10:59:15 +0800

    preview icon component

---
+++ example/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import IconsPage from './components/icons-page.vue'
+</script>
+
+<template>
+  <main>
+    <icons-page></icons-page>
+  </main>
+</template>
+
+<style>
+.block-box {
+  border: 2px solid grey;
+  padding: 10px;
+  margin: 20px 0;
+}
+</style>
---
+++ example/src/components/icons-page.vue
@@ -0,0 +1,22 @@
+<script setup>
+const handleClick = (e) => {
+  console.log("icon 点击事件：", e);
+};
+</script>
+
+<template>
+  <div class="block-box">
+    <span>基础 Icon </span>
+    <u-icon name="refresh" @click="handleClick"></u-icon>
+    <u-icon name="close" @click="handleClick"></u-icon>
+    <hr>
+    <span>hover 变色 </span>
+    <u-icon name="plus" @click="handleClick" class="icon-plus"></u-icon>
+  </div>
+</template>
+
+<style scoped>
+.icon-plus:hover {
+    fill: red;
+}
+</style>
