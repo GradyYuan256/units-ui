844673840355da42ab9c6a86c6236813be627f3f
GradyYuan256 <jackyuan256@gmail.com>
2022-02-26 12:04:33 +0800

    add switch

--- example/src/App.vue
+++ example/src/App.vue
@@ -3,6 +3,7 @@ import IconsPage from './components/icons-page.vue'
 import ButtonPage from './components/button-page.vue'
 import RadiosPage from './components/radios-page.vue'
 import CheckboxPage from './components/checkbox-page.vue'
+import SwitchPage from './components/switch-page.vue'
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import CheckboxPage from './components/checkbox-page.vue'
     <button-page></button-page>
     <radios-page></radios-page>
     <checkbox-page></checkbox-page>
+    <switch-page></switch-page>
   </main>
 </template>
 
---
+++ example/src/components/switch-page.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, watch } from "vue";
+// import USwitch from "../components/switch/index.vue";
+
+let switchValue = ref(true);
+let switchValue1 = ref(true);
+const handleChange = (e) => {
+  console.log("switch 点击事件：", e);
+};
+const handleChange1 = (e) => {
+  console.log("形状 switch 点击事件：", e);
+};
+
+watch(() => switchValue.value, (newV, oldV) => {
+  console.log('switch watch -->', newV, oldV);
+})
+</script>
+
+<template>
+  <div class="block-box">
+    <span>基础 Switch </span>
+    <u-switch v-model:value="switchValue" @update:value="handleChange"></u-switch>
+    <u-switch v-model:value="switchValue" disabled></u-switch>
+    <hr />
+    <span>形状 </span>
+    <u-switch v-model:value="switchValue1" @update:value="handleChange1" square></u-switch>
+    <u-switch v-model:value="switchValue1" disabled square></u-switch>
+    <hr />
+    <hr />
+  </div>
+</template>
+
